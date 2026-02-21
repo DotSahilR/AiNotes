be24f1013e77a98c3d612dbb96d5a2be242ec7fc
Sahil Reavnker <162610734+DotSahilR@users.noreply.github.com>
2026-02-21 12:46:09 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Enables the App Directory feature
+  },
 };
 
 export default nextConfig;
